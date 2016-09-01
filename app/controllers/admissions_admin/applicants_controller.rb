f65b0172a013667ecf5cfdacdd3dddad36cee96e
Alf <alf1337@gmail.com>
2016-09-01 16:29:40 +0200

    At interested in other positions show which group the application was for (#133)

--- app/controllers/admissions_admin/applicants_controller.rb
+++ app/controllers/admissions_admin/applicants_controller.rb
@@ -1,10 +1,6 @@
 class AdmissionsAdmin::ApplicantsController < ApplicationController
   def show_interested_other_positions
     @admission = Admission.find(params[:admission_id])
-    if @admission.admin_priority_deadline > Time.current
-      return @applicants = []
-    else
-      @applicants = Applicant.interested_other_positions(@admission)
-    end
+    @applicants = Applicant.interested_other_positions(@admission)
   end
 end
