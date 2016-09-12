7ab57fa1bf9aead06f316f9ef774f61a117069b4
Erlend Eriksen <erlend_ne@hotmail.com>
2016-09-12 21:39:51 +0200

    validate email in sulten

--- app/models/sulten/reservation.rb
+++ app/models/sulten/reservation.rb
@@ -12,7 +12,7 @@ class Sulten::Reservation < ActiveRecord::Base
 
   validate :check_opening_hours, :reservation_is_one_day_in_future, :check_amount_of_people, on: :create
 
-  validate :email, email: true
+  validates :email, email: true
 
   before_validation(on: :create) do
     should_break = false
